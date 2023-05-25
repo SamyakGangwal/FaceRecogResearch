d4474fdb58b144f07409345d020001a3ea12a248
SamyakGangwal <sam.gangwal97@gmail.com>
2023-05-25 06:52:09 +0530

    Initial Commit

---
+++ FaceRecogResearch/FaceRecogResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecogResearchApp.swift
+//  FaceRecogResearch
+//
+//  Created by GA on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceRecogResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
